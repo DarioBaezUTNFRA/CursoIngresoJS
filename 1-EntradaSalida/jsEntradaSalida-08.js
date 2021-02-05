9fcceb9afce05ad7b12d09879fc2e1779846b474
Dario Baez <DBaez.externo@coelsa.com.ar>
2021-02-05 00:42:47 -0300

    Ejercicio 8

--- 1-EntradaSalida/jsEntradaSalida-08.js
+++ 1-EntradaSalida/jsEntradaSalida-08.js
@@ -4,7 +4,6 @@ Debemos lograr tomar Los numeros por ID ,
 transformarlos a enteros (parseInt),realizar la operación correcta y 
 mostrar el resto entre el dividendo y el divisor.
 ej.: "El resto es 0 ."*/
-
 /* Ejercicio 8 */
 function SacarResto()
 {
