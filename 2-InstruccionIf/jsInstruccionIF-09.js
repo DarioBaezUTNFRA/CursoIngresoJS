e808f3609b745498130f87801cb7f4281dd9db04
Dario Baez <DBaez.externo@coelsa.com.ar>
2021-02-10 19:18:12 -0300

    InstruccionIF-09

--- 2-InstruccionIf/jsInstruccionIF-09.js
+++ 2-InstruccionIf/jsInstruccionIF-09.js
@@ -8,7 +8,7 @@ function mostrar()
 	var numeroRandom;
 	
 	//Genero el número RANDOM entre 1 y 10 inclusive
-	numeroRandom = Math.floor(Math.random() * (10 - 1 + 1) ) + 1;
+	numeroRandom = Math.floor(Math.random() * 10 ) + 1;
 	
 	//mostrar un número Random del 1 al 10 inclusive
     console.log("El número random es: " + numeroRandom);  
