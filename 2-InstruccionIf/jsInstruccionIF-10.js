0022a2d2055efa4ee86a9e6884eb10ec4ffec94d
Dario Baez <DBaez.externo@coelsa.com.ar>
2021-02-10 19:29:03 -0300

    InstruccionIF-10

--- 2-InstruccionIf/jsInstruccionIF-10.js
+++ 2-InstruccionIf/jsInstruccionIF-10.js
@@ -10,28 +10,27 @@ function mostrar()
 {   
 	//declaracion de variables
 	var nota;
-    
+    var mensaje;
+	
 	//asignar una nota RANDOM al examen entre 1 y 10 inclusive
     nota = Math.floor(Math.random() * 10 ) + 1;
-
-    console.log("nota: " + nota);
 
 	//para notas menores a 4 
 	if( nota < 4)
 	{   
 		//mostrar "Vamos, la proxima se puede"
-		alert("Vamos, la proxima se puede");
+		mensaje = "La nota es: " + nota + " Vamos, la proxima se puede";
 	}
 	else 
 	{  
 		if(nota > 8) //para notas igual a 9 o 10
 		{
-			alert("EXCELENTE");
-
+			mensaje = "La nota es: " + nota + " EXCELENTE";
 		}
 		else // para notas mayores a 4
-		{
-			alert("APROBÓ");
+		{   
+			mensaje = "La nota es: " + nota + " APROBÓ";
 		}
 	}
+	alert(mensaje);
 }//FIN DE LA FUNCIÓN
