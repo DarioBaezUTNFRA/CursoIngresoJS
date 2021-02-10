dda8b4595cb5c6b3b42159949219421dc679a696
Dario Baez <DBaez.externo@coelsa.com.ar>
2021-02-10 01:03:27 -0300

    InstruccionIF-09

--- 2-InstruccionIf/jsInstruccionIF-09.js
+++ 2-InstruccionIf/jsInstruccionIF-09.js
@@ -1,6 +1,16 @@
+/* Dario Baez */
+
+/* Al presionar el Botón, mostrar un número Random del 1 al 10 inclusive*/
+
+/* Ej 9 IF */
 function mostrar()
 {
-	//Genero el número RANDOM entre 1 y 10 
-	alert("ok");	
+	var numeroRandom;
+	
+	//Genero el número RANDOM entre 1 y 10 inclusive
+	numeroRandom = Math.floor(Math.random() * (10 - 1 + 1) ) + 1;
+	
+	//mostrar un número Random del 1 al 10 inclusive
+    console.log("El número random es: " + numeroRandom);  
 
 }//FIN DE LA FUNCIÓN
