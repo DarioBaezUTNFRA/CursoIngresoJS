e1d7389ca14422986a0b1726b8e9726517d35fc5
Dario Baez <DBaez.externo@coelsa.com.ar>
2021-02-11 20:38:39 -0300

    InstruccionSwitch-05

--- 3-InstruccionSwitch/jsInstruccionSwitch-05.js
+++ 3-InstruccionSwitch/jsInstruccionSwitch-05.js
@@ -1,11 +1,32 @@
+/* Dario Baez */
+
+/* Al ingresar una hora, informar:
+si está entre las 7 y las 11 : "Es de mañana.".*/
+
+/* EJ 05 Switch*/
 function mostrar()
 {
-	//tomo la hora
-	var horaDelDia =txtIdHora.value;
-	alert(horaDelDia);
+	///declaracion de variables
+	var hora;
+
+	//tomar datos ingresados por ID
+	hora = document.getElementById('txtIdHora').value;
+    
+	//parseInt
+	hora = parseInt(hora);
+
+	switch (hora)
+	{   
+		case 7:
+		case 8:
+		case 9:
+		case 10:
+		case 11:
+			alert("Es de mañana.");
+			break;
+		default:
+			console.log("Hora: " + hora);
+			break;
+	}
 	
-	
-
-
-
 }//FIN DE LA FUNCIÓN
