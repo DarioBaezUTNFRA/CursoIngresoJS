f00508a829d9acf4c9ea26dba934a34f9219e458
Dario Baez <DBaez.externo@coelsa.com.ar>
2021-02-11 19:57:26 -0300

    InstruccionSwitch-03

--- 3-InstruccionSwitch/jsInstruccionSwitch-03.js
+++ 3-InstruccionSwitch/jsInstruccionSwitch-03.js
@@ -1,10 +1,32 @@
+/* Dario Baez */
+
+/* al seleccionar un mes informar.
+si es Febrero: " Este mes no tiene más de 29 días."
+si NO es Febrero: "Este mes tiene 30 o más días"*/
+
+/* EJ 03  Switch*/
 function mostrar()
 {
-	//tomo el mes
-	var mesDelAño =txtIdMes.value;
-	alert(mesDelAño);
-	
-	
+	///declaracion de variables
+	var mesDelAño;
+	var mensaje;
+    
+	//tomar datos ingresados por ID
+	mesDelAño = document.getElementById('txtIdMes').value;
 
-
+	switch (mesDelAño)
+	{
+		case "Febrero":
+		{   
+			mensaje = "Este mes no tiene más de 29 días.";
+			break;
+		}
+		default:
+		{
+			mensaje = "Este mes tiene 30 o más días."
+			break;
+		}
+	}
+	alert(mensaje);
+	
 }//FIN DE LA FUNCIÓN
