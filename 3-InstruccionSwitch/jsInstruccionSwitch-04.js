37221035fa9235c5f09c39bf5678abeee6e43dc1
Dario Baez <DBaez.externo@coelsa.com.ar>
2021-02-11 20:08:18 -0300

    InstruccionSwitch-04

--- 3-InstruccionSwitch/jsInstruccionSwitch-04.js
+++ 3-InstruccionSwitch/jsInstruccionSwitch-04.js
@@ -1,11 +1,41 @@
+/* Dario Baez */
+
+/* al seleccionar un mes informar.
+si tiene 28 días.
+si tiene 30 días.
+si tiene 31 días.*/
+
+/* EJ 04  Switch*/
 function mostrar()
 {
-	//tomo el mes
-	var mesDelAño =txtIdMes.value;
-	alert(mesDelAño);
+	///declaracion de variables
+	var mesDelAño;
+	var mensaje;
+    
+	//tomar datos ingresados por ID
+	mesDelAño = document.getElementById('txtIdMes').value;
+
+	switch (mesDelAño)
+	{   
+		case "Febrero":
+		{   
+			mensaje = "Este mes tiene 28 días.";
+			break;
+		}
+		case "Abril":
+		case "Junio":
+		case "Septiembre":
+		case "Noviembre":
+		{
+			mensaje = "Este mes tiene 30 días.";
+			break;
+		}
+		default:
+		{
+			mensaje = "Este mes tiene 31 días."
+			break;
+		}
+	}
+	alert(mensaje);
 	
-	
-
-
-
 }//FIN DE LA FUNCIÓN
