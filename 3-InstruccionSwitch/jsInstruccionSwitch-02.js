c7bfc7f3208039e84eb81cd96561945f6d2b0f4d
Dario Baez <DBaez.externo@coelsa.com.ar>
2021-02-11 19:44:26 -0300

    InstruccionSwitch-02

--- 3-InstruccionSwitch/jsInstruccionSwitch-02.js
+++ 3-InstruccionSwitch/jsInstruccionSwitch-02.js
@@ -1,10 +1,46 @@
+/* Dario Baez */
+
+/* al seleccionar un mes informar.
+si estamos en Invierno: "Abrigate que hace frio."
+si aún no llego el Invierno: "Falta para el invierno."
+si ya paso el Invierno: "Ya pasamos el frio, ahora calor!!!."
+ACLARAcIÓN: tomamos a Julio y Agosto como los meses de Invierno.*/
+
+/* EJ 02  Switch*/
 function mostrar()
 {
-	//tomo el mes
-	var mesDelAño =txtIdMes.value;
-	alert(mesDelAño);
+	///declaracion de variables
+	var mesDelAño;
+	var mensaje;
+    
+	//tomar datos ingresados por ID
+	mesDelAño = document.getElementById('txtIdMes').value;
 
+	switch (mesDelAño)
+	{
+		case "Enero":
+		case "Febrero":
+		case "Marzo":
+		case "Abril":
+		case "Mayo":
+		case "Junio":
+		{   
+			mensaje = "Falta para el invierno.";
+			break;
+		}
+		case "Julio":
+		case "Agosto":
+		{
+			mensaje = "Abrigate que hace frio.";
+			break;
+		}
+		default:
+		{
+			mensaje = "Ya pasamos el frio, ahora calor!!!."
+			break;
+		}
+	}
 
-
-
+	alert(mensaje);
+	
 }//FIN DE LA FUNCIÓN
