b4a9572446fa995ebf01296aad156740e4fd0b59
Dario Baez <DBaez.externo@coelsa.com.ar>
2021-02-12 20:41:33 -0300

    IteracionesWhile-04

--- 5-InstruccionWhile/jsIteracionesWhile-04.js
+++ 5-InstruccionWhile/jsIteracionesWhile-04.js
@@ -1,9 +1,22 @@
-/*
-al presionar el botón 
-pedir un número entre 0 y 9 inclusive.*/
+/* Dario Baez */
+
+/* al presionar el botón  pedir un número entre 0 y 9 inclusive.*/
+
+/*EJ While 04*/
 function mostrar()
-{
-	var numeroIngresado;
-	numeroIngresado = prompt("ingrese un número entre 0 y 10.");
+{   
+	//declaracion de variables
+	var numeroIngresado;  
+
+	//ingreso de datos
+	numeroIngresado = prompt("ingrese un número entre 0 y 9 inclusive.");
+
+	//parseInt
+	numeroIngresado = parseInt(numeroIngresado);
+
+	while(numeroIngresado < 0 || numeroIngresado > 9)
+	{
+		numeroIngresado = prompt("ingrese un número entre 0 y 9 inclusive.");
+	}
 	
 }//FIN DE LA FUNCIÓN
