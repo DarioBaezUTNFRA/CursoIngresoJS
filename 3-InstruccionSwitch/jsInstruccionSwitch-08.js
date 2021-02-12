08704b9978cfb2eb02aa31bc2d1580e6c2985893
Dario Baez <DBaez.externo@coelsa.com.ar>
2021-02-11 21:13:17 -0300

    InstruccionSwitch-08

--- 3-InstruccionSwitch/jsInstruccionSwitch-08.js
+++ 3-InstruccionSwitch/jsInstruccionSwitch-08.js
@@ -1,6 +1,28 @@
+/* Dario Baez */
+
+/* Al seleccionar un destino informar si hace FRIO o CALOR en ese destino*/
+
+/* EJ 08 Switch*/
 function mostrar()
 {
-	var destinoIngresado =txtIdDestino.value;
-	alert(destinoIngresado);
+	///declaracion de variables
+	var destino;
+    var temperatura;
+
+	//tomar datos ingresados por ID
+	destino = document.getElementById('txtIdDestino').value;
+    
+	switch (destino)
+	{   
+		case "Bariloche":
+		case "Ushuaia":
+			temperatura = "FRIO";
+			break;
+		default:
+			temperatura = "CALOR";
+			break;
+	}
+
+	alert("En "+ destino + " hace " + temperatura);
 
 }//FIN DE LA FUNCIÓN
