5f3c07feaae724253396cf62adb663d4b365be3d
Dario Baez <DBaez.externo@coelsa.com.ar>
2021-02-11 21:07:10 -0300

    InstruccionSwitch-07

--- 3-InstruccionSwitch/jsInstruccionSwitch-07.js
+++ 3-InstruccionSwitch/jsInstruccionSwitch-07.js
@@ -1,6 +1,32 @@
+/* Dario Baez */
+
+/* Al selecionar un destino , indicar el punto cardinal de nuestro pais en donde se encuentra Norte, Sur, Este u Oeste*/
+
+/* EJ 07 Switch*/
 function mostrar()
 {
-	var destinoIngresado =txtIdDestino.value;
-	alert(destinoIngresado);
+	///declaracion de variables
+	var destino;
+    var puntoCardinal;
+	//tomar datos ingresados por ID
+	destino = document.getElementById('txtIdDestino').value;
+    
+	switch (destino)
+	{   
+		case "Bariloche":
+			puntoCardinal = "Oeste";
+			break;
+		case "Cataratas":
+			puntoCardinal = "Norte";
+			break;
+		case "Mar del plata":
+			puntoCardinal = "Este";
+			break;
+		case "Ushuaia":
+			puntoCardinal = "Sur";
+			break;
+	}
+
+	alert(destino + " se encuentra al " + puntoCardinal + " de nuestro pais.");
 
 }//FIN DE LA FUNCIÓN
